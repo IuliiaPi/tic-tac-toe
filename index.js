0335e9669c4385047fceaca6dd3461606dac12cb
IuliiaPi <julia.pidlisna@gmail.com>
2023-10-13 12:26:59 -0400

    feat: add win combs

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const btnOptionO = document.querySelector('.option__o');
 const o = '0';
 const x = 'x';
 
-let currentPlayer;
+let currentPlayer = x;
+// let turn_O;
 
-const cells = document.querySelectorAll('.cell');
+let cells = document.querySelectorAll('.cell');
 const cell_0 = document.querySelector('.cell-0');
 const cell_1 = document.querySelector('.cell-1');
 const cell_2 = document.querySelector('.cell-2');
@@ -17,13 +18,45 @@ const cell_6 = document.querySelector('.cell-6');
 const cell_7 = document.querySelector('.cell-7');
 const cell_8 = document.querySelector('.cell-8');
 
+const winCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
 const buttonsRestart = document.querySelectorAll('.btn-restart');
+
 const popUpGameStart = document.querySelector('.pop-up_game-start');
 const popUpGameOver = document.querySelector('.pop-up_game-over');
 popUpGameOver.classList.add('_hidden');
 
 const messageResult = document.querySelector('.message-result');
+const clicks = document.querySelector('.click-count');
 
+const subtitlePlayer = document.querySelector('.subtitle-player');
+const subtitleClicks = document.querySelector('.subtitle-clicks');
+
+// Counter
+let clickCount = 0;
+
+function Counter() {
+    clickCount++;
+    clicks.innerHTML = clickCount;
+    subtitleClicks.innerHTML = clickCount;
+}
+
+// resetCounter
+function resetCounter() {
+    clickCount = 0;
+    clicks.innerHTML = clickCount;
+}
+
+// audio
 const audio = new Audio();
 let isPlay = false;
 
@@ -35,171 +68,175 @@ function playAudio() {
     isPlay = true;
 }
 
-// currentPlayer = "X";
-
-// function click_X_O() {
-//     this.innerHTML = currentPlayer;
-
-//     if (currentPlayer.innerHTML === 'X') {
-//         currentPlayer.innerHTML = 'O';
-//     } else {
-//         currentPlayer.innerHTML = 'X';
-//     }
+// for (let i = 0; i < cells.length; i++) {
+//     cells[i].addEventListener('click', cellClick);
 // }
-// this.removeEventListener('click', click_X_O);
-
 
 cells.forEach(cell => {
+    cell.addEventListener('click', cellClick);
+});
 
-    // cell.addEventListener('click', click_X_O);
-
-
-    function cellClick_o() {
-        currentPlayer = 'O';
-        messageResult.innerHTML = `Player ${currentPlayer} has won!`;
-
-        cell.innerHTML = o;
-        playAudio();
-
-
-        if ((cell_0.innerHTML === o) & (cell_1.innerHTML === o) & (cell_2.innerHTML === o)) {
-            cell_0.classList.add('_active');
-            cell_1.classList.add('_active');
-            cell_2.classList.add('_active');
-            popUpGameOver.classList.remove('_hidden');
-        } else if ((cell_3.innerHTML === o) & (cell_4.innerHTML === o) & (cell_5.innerHTML === o)) {
-            cell_3.classList.add('_active');
-            cell_4.classList.add('_active');
-            cell_5.classList.add('_active');
-            popUpGameOver.classList.remove('_hidden');
-        } else if ((cell_6.innerHTML === o) & (cell_7.innerHTML === o) & (cell_8.innerHTML === o)) {
-            cell_6.classList.add('_active');
-            cell_7.classList.add('_active');
-            cell_8.classList.add('_active');
-            popUpGameOver.classList.remove('_hidden');
-        } else if ((cell_0.innerHTML === o) & (cell_3.innerHTML === o) & (cell_6.innerHTML === o)) {
-            cell_0.classList.add('_active');
-            cell_3.classList.add('_active');
-            cell_6.classList.add('_active');
-            popUpGameOver.classList.remove('_hidden');
-        } else if ((cell_1.innerHTML === o) & (cell_4.innerHTML === o) & (cell_7.innerHTML === o)) {
-            cell_1.classList.add('_active');
-            cell_4.classList.add('_active');
-            cell_7.classList.add('_active');
-            popUpGameOver.classList.remove('_hidden');
-        } else if ((cell_2.innerHTML === o) & (cell_5.innerHTML === o) & (cell_8.innerHTML === o)) {
-            cell_2.classList.add('_active');
-            cell_5.classList.add('_active');
-            cell_8.classList.add('_active');
-            popUpGameOver.classList.remove('_hidden');
-        } else if ((cell_0.innerHTML === o) & (cell_4.innerHTML === o) & (cell_8.innerHTML === o)) {
-            cell_0.classList.add('_active');
-            cell_4.classList.add('_active');
-            cell_8.classList.add('_active');
-            popUpGameOver.classList.remove('_hidden');
-        } else if ((cell_2.innerHTML === o) & (cell_4.innerHTML === o) & (cell_6.innerHTML === o)) {
-            cell_2.classList.add('_active');
-            cell_4.classList.add('_active');
-            cell_6.classList.add('_active');
-            popUpGameOver.classList.remove('_hidden');
-        }
-
-    };
-
-    function cellClick_x() {
-        currentPlayer = 'X';
-        messageResult.innerHTML = `Player ${currentPlayer} has won!`;
-
-        cell.innerHTML = x;
-        playAudio();
-        // click_XO();
-
-        if ((cell_0.innerHTML === x) & (cell_1.innerHTML === x) & (cell_2.innerHTML === x)) {
-            cell_0.classList.add('_active');
-            cell_1.classList.add('_active');
-            cell_2.classList.add('_active');
-            popUpGameOver.classList.remove('_hidden');
-        } else if ((cell_3.innerHTML === x) & (cell_4.innerHTML === x) & (cell_5.innerHTML === x)) {
-            cell_3.classList.add('_active');
-            cell_4.classList.add('_active');
-            cell_5.classList.add('_active');
-            popUpGameOver.classList.remove('_hidden');
-        } else if ((cell_6.innerHTML === x) & (cell_7.innerHTML === x) & (cell_8.innerHTML === x)) {
-            cell_6.classList.add('_active');
-            cell_7.classList.add('_active');
-            cell_8.classList.add('_active');
-            popUpGameOver.classList.remove('_hidden');
-        } else if ((cell_0.innerHTML === x) & (cell_3.innerHTML === x) & (cell_6.innerHTML === x)) {
-            cell_0.classList.add('_active');
-            cell_3.classList.add('_active');
-            cell_6.classList.add('_active');
-            popUpGameOver.classList.remove('_hidden');
-        } else if ((cell_1.innerHTML === x) & (cell_4.innerHTML === x) & (cell_7.innerHTML === x)) {
-            cell_1.classList.add('_active');
-            cell_4.classList.add('_active');
-            cell_7.classList.add('_active');
-            popUpGameOver.classList.remove('_hidden');
-        } else if ((cell_2.innerHTML === x) & (cell_5.innerHTML === x) & (cell_8.innerHTML === x)) {
-            cell_2.classList.add('_active');
-            cell_5.classList.add('_active');
-            cell_8.classList.add('_active');
-            popUpGameOver.classList.remove('_hidden');
-        } else if ((cell_0.innerHTML === x) & (cell_4.innerHTML === x) & (cell_8.innerHTML === x)) {
-            cell_0.classList.add('_active');
-            cell_4.classList.add('_active');
-            cell_8.classList.add('_active');
-            popUpGameOver.classList.remove('_hidden');
-        } else if ((cell_2.innerHTML === x) & (cell_4.innerHTML === x) & (cell_6.innerHTML === x)) {
-            cell_2.classList.add('_active');
-            cell_4.classList.add('_active');
-            cell_6.classList.add('_active');
-            popUpGameOver.classList.remove('_hidden');
-        }
-    };
-
-
-
-    btnOptionX.addEventListener('click', () => {
-        popUpGameStart.classList.add('_hidden');
-        cell.addEventListener('click', cellClick_x);
-    });
-
-    btnOptionO.addEventListener('click', () => {
-        popUpGameStart.classList.add('_hidden');
-        cell_0.innerHTML = x;
-        cell.addEventListener('click', cellClick_o);
-    });
-
-
-    function setLocalStorage() {
-        localStorage.setItem('enterText', cell.innerHTML);
+// cellClick
+function cellClick() {
+    this.innerHTML = currentPlayer;
+    // currentPlayer === x ? o : x;
+    if (currentPlayer === x) {
+        currentPlayer = o;
+    } else {
+        currentPlayer = x;
     }
-    window.addEventListener('beforeunload', setLocalStorage);
+    this.removeEventListener('click', cellClick);
 
-    function getLocalStorage() {
-        if (localStorage.getItem('enterText')) {
-            cell.innerHTML = localStorage.getItem('enterText');
+    cellClick_o();
+    cellClick_x();
+    playAudio();
+    Counter();
+}
 
-            restartGame();
-        }
-    }
-    window.addEventListener('load', getLocalStorage);
+// function cellClick(e) {
+//   const cell = e.target;
+//   let currentPlayer;
+//   currentPlayer = turn_O ? o : x;
+//   placeMark(cell, currentPlayer);
+//   swapTurns();
+// }
 
+// function placeMark(cell, currentPlayer) {
+//     cell.classList.add(currentPlayer.innerHTML);
+// }
+
+// function swapTurns() {
+//     turn_O = !turn_O;
+// }
+
+btnOptionX.addEventListener('click', () => {
+    popUpGameStart.classList.add('_hidden');
+});
+
+btnOptionO.addEventListener('click', () => {
+    popUpGameStart.classList.add('_hidden');
+    // cell_1.innerHTML = x;
 });
 
 
-// if (cell_0.classList.contains('_active')) {
-//     // cells.forEach(cell => {
-//     cell_0.classList.add('_active-3');
-//     // });
-// }
+function win_o() {
+    popUpGameOver.classList.remove('_hidden');
+    messageResult.innerHTML = `Player ${o} has won!`;
+    subtitlePlayer.innerHTML = o;
+}
 
+function win_x() {
+    popUpGameOver.classList.remove('_hidden');
+    messageResult.innerHTML = `Player ${x} has won!`;
+    subtitlePlayer.innerHTML = x;
+}
 
+function draw() {
+    popUpGameOver.classList.remove('_hidden');
+    messageResult.innerHTML = `Game ended in a draw!`;
+    subtitlePlayer.innerHTML = '';
+}
+
+function cellClick_o() {
+    if ((cell_0.innerHTML === o) & (cell_1.innerHTML === o) & (cell_2.innerHTML === o)) {
+        cell_0.classList.add('_active');
+        cell_1.classList.add('_active');
+        cell_2.classList.add('_active');
+        win_o();
+    } else if ((cell_3.innerHTML === o) & (cell_4.innerHTML === o) & (cell_5.innerHTML === o)) {
+        cell_3.classList.add('_active');
+        cell_4.classList.add('_active');
+        cell_5.classList.add('_active');
+        win_o();
+    } else if ((cell_6.innerHTML === o) & (cell_7.innerHTML === o) & (cell_8.innerHTML === o)) {
+        cell_6.classList.add('_active');
+        cell_7.classList.add('_active');
+        cell_8.classList.add('_active');
+        win_o();
+    } else if ((cell_0.innerHTML === o) & (cell_3.innerHTML === o) & (cell_6.innerHTML === o)) {
+        cell_0.classList.add('_active');
+        cell_3.classList.add('_active');
+        cell_6.classList.add('_active');
+        win_o();
+    } else if ((cell_1.innerHTML === o) & (cell_4.innerHTML === o) & (cell_7.innerHTML === o)) {
+        cell_1.classList.add('_active');
+        cell_4.classList.add('_active');
+        cell_7.classList.add('_active');
+        win_o();
+    } else if ((cell_2.innerHTML === o) & (cell_5.innerHTML === o) & (cell_8.innerHTML === o)) {
+        cell_2.classList.add('_active');
+        cell_5.classList.add('_active');
+        cell_8.classList.add('_active');
+        win_o();
+    } else if ((cell_0.innerHTML === o) & (cell_4.innerHTML === o) & (cell_8.innerHTML === o)) {
+        cell_0.classList.add('_active');
+        cell_4.classList.add('_active');
+        cell_8.classList.add('_active');
+        win_o();
+    } else if ((cell_2.innerHTML === o) & (cell_4.innerHTML === o) & (cell_6.innerHTML === o)) {
+        cell_2.classList.add('_active');
+        cell_4.classList.add('_active');
+        cell_6.classList.add('_active');
+        win_o();
+    } else if (clickCount === 9) {
+        draw();
+    }
+};
+
+function cellClick_x() {
+    if ((cell_0.innerHTML === x) & (cell_1.innerHTML === x) & (cell_2.innerHTML === x)) {
+        cell_0.classList.add('_active');
+        cell_1.classList.add('_active');
+        cell_2.classList.add('_active');
+        win_x();
+    } else if ((cell_3.innerHTML === x) & (cell_4.innerHTML === x) & (cell_5.innerHTML === x)) {
+        cell_3.classList.add('_active');
+        cell_4.classList.add('_active');
+        cell_5.classList.add('_active');
+        win_x();
+    } else if ((cell_6.innerHTML === x) & (cell_7.innerHTML === x) & (cell_8.innerHTML === x)) {
+        cell_6.classList.add('_active');
+        cell_7.classList.add('_active');
+        cell_8.classList.add('_active');
+        win_x();
+    } else if ((cell_0.innerHTML === x) & (cell_3.innerHTML === x) & (cell_6.innerHTML === x)) {
+        cell_0.classList.add('_active');
+        cell_3.classList.add('_active');
+        cell_6.classList.add('_active');
+        win_x();
+    } else if ((cell_1.innerHTML === x) & (cell_4.innerHTML === x) & (cell_7.innerHTML === x)) {
+        cell_1.classList.add('_active');
+        cell_4.classList.add('_active');
+        cell_7.classList.add('_active');
+        win_x();
+    } else if ((cell_2.innerHTML === x) & (cell_5.innerHTML === x) & (cell_8.innerHTML === x)) {
+        cell_2.classList.add('_active');
+        cell_5.classList.add('_active');
+        cell_8.classList.add('_active');
+        win_x();
+    } else if ((cell_0.innerHTML === x) & (cell_4.innerHTML === x) & (cell_8.innerHTML === x)) {
+        cell_0.classList.add('_active');
+        cell_4.classList.add('_active');
+        cell_8.classList.add('_active');
+        win_x();
+    } else if ((cell_2.innerHTML === x) & (cell_4.innerHTML === x) & (cell_6.innerHTML === x)) {
+        cell_2.classList.add('_active');
+        cell_4.classList.add('_active');
+        cell_6.classList.add('_active');
+        win_x();
+    } else if (clickCount === 9) {
+        draw();
+    }
+};
+
+// buttonsRestart
 buttonsRestart.forEach(button => {
     button.addEventListener('click', () => {
         restartGame();
+        resetCounter();
         popUpGameOver.classList.add('_hidden');
         popUpGameStart.classList.remove('_hidden');
+        currentPlayer = x;
     });
 });
 
@@ -207,11 +244,36 @@ function restartGame() {
     cells.forEach(cell => {
         cell.innerHTML = '';
         cell.classList.remove('_active');
+        cell.addEventListener('click', cellClick);
     });
 }
 
 
+// function setLocalStorage() {
+//     localStorage.setItem('fillCell', cell.innerHTML);
+// }
+// window.addEventListener('beforeunload', setLocalStorage);
 
-// const winningMessage = () => `Player ${currentPlayer} has won!`;
-// const drawMessage = () => `Game ended in a draw!`;
-// const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
+// function getLocalStorage() {
+//     if (localStorage.getItem('fillCell')) {
+//         cell.innerHTML = localStorage.getItem('fillCell');
+
+//         restartGame();
+//     }
+// }
+// window.addEventListener('load', getLocalStorage);
+
+
+
+// function setLocalStorage() {
+//     localStorage.setItem('clickCount', clicks.innerHTML);
+// }
+// window.addEventListener('beforeunload', setLocalStorage);
+
+// function getLocalStorage() {
+//     if (localStorage.getItem('clickCount')) {
+//         clicks.innerHTML = localStorage.getItem('clickCount');
+//     }
+// }
+// window.addEventListener('load', getLocalStorage);
+
